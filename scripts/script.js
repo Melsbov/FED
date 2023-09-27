8c0717355b852c5b9b0c331cfdd4971ed614a87a
Melsbov <144225573+Melsbov@users.noreply.github.com>
2023-09-27 11:05:12 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,28 @@
 // JavaScript Document
 console.log("hi");
+
+var menu = document.querySelector('.menu');
+var items = document.querySelector('.items');
+var close = document.querySelector('.items section button');
+var body = document.querySelector('body');
+var div = document.querySelector('body>div');
+var computer = document.querySelector('.computer');
+var computerul = document.querySelector('.computerul');
+var allecategorieën = document.querySelector('.computerul button');
+
+menu.addEventListener('click', openMenu);
+close.addEventListener('click', openMenu );
+computer.addEventListener('click', openExtraMenu);
+allecategorieën.addEventListener('click', openExtraMenu);
+
+
+function openMenu(){
+    items.classList.toggle('visible');
+    body.classList.toggle('scroll');
+    div.classList.toggle('dark');
+    computerul.classList.remove('extraVisible');
+}
+
+function openExtraMenu(){
+    computerul.classList.toggle('extraVisible');
+}
