8111c50772f981074837877047cb2ac19ab7ca78
Melsbov <144225573+Melsbov@users.noreply.github.com>
2023-10-04 16:56:21 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,9 @@ var computer = document.querySelector('.computer');
 var computerul = document.querySelector('.computerul');
 var allecategorieën = document.querySelector('.computerul button');
 
+
 menu.addEventListener('click', openMenu);
-close.addEventListener('click', openMenu );
+close.addEventListener('click', openMenu);
 computer.addEventListener('click', openExtraMenu);
 allecategorieën.addEventListener('click', openExtraMenu);
 
@@ -25,4 +26,49 @@ function openMenu(){
 
 function openExtraMenu(){
     computerul.classList.toggle('extraVisible');
+    items.classList.toggle('scroll');
 }
+
+
+//
+//
+// Scroll buttons
+//
+//
+
+var buttonleft = document.querySelector('.buttonleft');
+var buttonright = document.querySelector('.buttonright');
+var ontdekul = document.querySelector('.ontdek>ul');
+
+buttonleft.addEventListener('click', scrollLeft);
+buttonright.addEventListener('click', scrollRight);
+
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollLeft
+function scrollLeft(){
+    ontdekul.scrollLeft -=200;
+}
+
+function scrollRight(){
+    ontdekul.scrollLeft +=200;
+}
+
+//
+//
+// MICROINTERACTIE
+//
+//
+
+var winkelwagenbutton = document.querySelector('.winkelwagenbutton');
+var notificatie = document.querySelector('main>span');
+
+
+winkelwagenbutton.addEventListener('click', microinteractie);
+
+
+function microinteractie(){
+    notificatie.classList.toggle('microinteractie');
+}
+
+
+
